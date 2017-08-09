ca95cbb4cbe249d7849f50f1bd59f6276e662ceb
Chris Luffingham <chris_luffingham@hotmail.com>
2017-08-09 15:28:07 +0100

    Added getDistance method to Util class

--- source/Util.hx
+++ source/Util.hx
@@ -7,9 +7,28 @@ class Util
 	static function get_instance(): Util return (instance == null) ? instance = new Util() : instance;
 	private function new() {}
 	
+	/**
+	 * Gets the angle between two points (vectors)
+	 * @param	a
+	 * @param	b
+	 * @return
+	 */
 	public function getAngle(a:FlxPoint, b:FlxPoint):Float
 	{
 		return Math.atan2(a.y - b.y, a.x - b.x);
+	}
+	
+	/**
+	 * Gets the distance between two points
+	 * @param	a
+	 * @param	b
+	 * @return
+	 */
+	public function getDistance(a:FlxPoint, b:FlxPoint):Float
+	{
+		var dx:Float = b.x - a.x;
+		var dy:Float = b.y - a.y;
+		return Math.sqrt(dx * dx + dy * dy);
 	}
 	
 }
